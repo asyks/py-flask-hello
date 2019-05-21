5fa1002aa55ef0581f4b886084af8094509d3c0d
asyks <aaron@aaronsykes.com>
2019-05-20 19:54:24 -0700

    Use werkzeug routing system

--- py_flask_hello/app.py
+++ py_flask_hello/app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from werkzeug.routing import Rule
 
 from . import constants
 
 
 app = Flask(__name__)
 
+app.url_map.add(Rule("/", methods=("HEAD", "GET"), endpoint="index"))
 
-@app.route("/")
+
+@app.endpoint("index")
 def hello():
     return constants.SUCCESS_MESSAGE
